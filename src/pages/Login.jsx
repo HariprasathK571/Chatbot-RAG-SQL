db5d9075394b3ed198f8531066aa9e362479243b
HariprasathK571 <hariprasathk571@gmail.com>
2026-07-03 14:34:34 +0530

    i have faced some problem in the login section due to the some format issue. i have solved that issue in this commit.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -31,10 +31,23 @@ export default function Login() {
     try {
       setLoading(true);
 
+      // const res = await fetch(LOGIN_URL, {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({ email: email.trim(), password }),
+      // });
+
+      const formData = new URLSearchParams();
+
+      formData.append("username", email.trim());
+      formData.append("password", password);
+
       const res = await fetch(LOGIN_URL, {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: email.trim(), password }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formData,
       });
 
       if (!res.ok) {
